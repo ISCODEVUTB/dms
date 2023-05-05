ff229579a67c38cb62bc2ea3805a78f9a93143dd
Adrian <avizcaino@utb.edu.co>
2023-05-05 13:09:12 -0500

    Uptate services to review

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
+from datetime import date
 from controller.BDController import DatabaseController
 from logic.classes.audiobook import AudioBook
 from logic.classes.book import Book
@@ -27,39 +28,42 @@ def read_root():
     return {"200": "Welcome To Document Restful API"}
 
 
-@app.get("/api/user")
+@app.get("/api/document/select")
 async def root():
     return bd_object.select_documents()
 
 
-@app.post("/api/document/ElectronicDoc/Book")
-async def add(author="author", title="title", price=100.0, topic="topic", language="language", pub_date="2000-01-01", publisher="publisher", editor="editor", pages=1, synopsis="synopsis", presentation="presentation"):
+@app.post("/api/document/ElectronicDoc/Add/Book")
+async def add_Book(author: str = "author", title: str = "title", price: float = 100.0, topic: str = "topic", language: str = "language", pub_date: date = date.today(), publisher: str = "publisher", editor: str = "editor", pages: int = 1, synopsis: str = "synopsis", presentation: str = "presentation"):
     return bd_object.insert_document(Book(author=author, title=title, price=price, topic=topic, language=language, pub_date=pub_date, publisher=publisher, editor=editor, pages=pages, synopsis=synopsis, presentation=presentation))
 
 
-@app.post("/api/document/ElectronicDoc/Audiobook")
-async def add(author="author", title="title", price=100.0, topic="topic", language="language", pub_date="2000-01-01", size=1, doi="doi", duration=1, synopsis="synopsis"):
+@app.post("/api/document/ElectronicDoc/Add/Audiobook")
+async def add_Audiobook(author: str = "author", title: str = "title", price: float = 100.0, topic: str = "topic", language: str = "language", pub_date: date = date.today(), size: float = 1, doi: str = "doi", duration: float = 1, synopsis: str = "synopsis"):
     return bd_object.insert_document(AudioBook(author=author, title=title, price=price, topic=topic, language=language, pub_date=pub_date, size=size, doi=doi, duration=duration, synopsis=synopsis))
 
 
-@app.post("/api/document/ElectronicDoc/Ebook")
-async def add(author="author", title="title", price=100.0, topic="topic", language="language", pub_date="2000-01-01", size=1, doi="doi", editor="editor", pages=1, synopsis="synopsis"):
+@app.post("/api/document/ElectronicDoc/Add/Ebook")
+async def add_Ebook(author: str = "author", title: str = "title", price: float = 100.0, topic: str = "topic", language: str = "language", pub_date: date = date.today(), size: float = 1, doi: str = "doi", editor: str = "editor", pages: int = 1, synopsis: str = "synopsis"):
     return bd_object.insert_document(Ebook(author=author, title=title, price=price, topic=topic, language=language, pub_date=pub_date, size=size, doi=doi, editor=editor, pages=pages, synopsis=synopsis))
 
 
-@app.post("/api/document/ElectronicDoc/Invbook")
-async def add(author="author", title="title", price=100.0, topic="topic", language="language", pub_date="2000-01-01", size=1, doi="doi", pages=1, abstract="abstract"):
+@app.post("/api/document/ElectronicDoc/Add/Invbook")
+async def add_Invbook(author: str = "author", title: str = "title", price: float = 100.0, topic: str = "topic", language: str = "language", pub_date: date = date.today(), size: float = 1, doi: str = "doi", pages: int = 1, abstract: str = "abstract"):
     return bd_object.insert_document(InvBook(author=author, title=title, price=price, topic=topic, language=language, pub_date=pub_date, size=size, doi=doi, pages=pages, abstract=abstract))
 
 
-@app.post("/api/document/ElectronicDoc/Magazine")
-async def add(author="author", title="title", price=100.0, topic="topic", language="language", pub_date="2000-01-01", size=1, doi="doi", edition=1, pages=1):
+@app.post("/api/document/ElectronicDoc/Add/Magazine")
+async def add_Magazine(author: str = "author", title: str = "title", price: float = 100.0, topic: str = "topic", language: str = "language", pub_date: date = date.today(), size: float = 1, doi: str = "doi", edition: int = 1, pages: int = 1):
     return bd_object.insert_document(Magazine(author=author, title=title, price=price, topic=topic, language=language, pub_date=pub_date, size=size, doi=doi, edition=edition, pages=pages))
 
 
-@app.delete("/api/user")
-async def delete(id_document: int):
-    return bd_object.delete_document(id_document)
+@app.delete("/api/document/delete/{id_document}/{table_name}")
+async def delete(id_document: int, table_name: str):
+    if table_name == "Books" or "Audiobooks" or "Ebooks" or "Magazines" or "Investigation_books":
+        return bd_object.delete_document(id_document, table_name)
+    else:
+        return "Table not found"
 
 
 if __name__ == "__main__":
--- controller/BDController.py
+++ controller/BDController.py
@@ -16,108 +16,79 @@ class DatabaseController():
     def insert_document(self, document: Book or Ebook or Magazine or AudioBook or InvBook):
 
         if isinstance(document, AudioBook):
-
-            self.cursor.execute('INSERT INTO All_documents (author, title, price, topic, language, pub_date) VALUES (?, ?, ?, ?, ?, ?)',
-                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date))
-
-            Prev_ID = self.cursor.lastrowid
-
-            self.cursor.execute('INSERT INTO Audiobooks (ID, duration, synopsis) VALUES (?, ?, ?)',
-                                (Prev_ID, document.duration, document.synopsis))
-
-            self.cursor.execute('INSERT INTO Electronic_documents (ID,author, title, price, topic, language, pub_date, size, doi) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                (Prev_ID, document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi))
+            self.cursor.execute('INSERT INTO Audiobooks (author, title, price, topic, language, pub_date, size, doi, duration, synopsis) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi, document.duration, document.synopsis))
             self.connection.commit()
 
             return "Document inserted successfully"
 
         elif isinstance(document, Ebook):
-
-            self.cursor.execute('INSERT INTO All_documents (author, title, price, topic, language, pub_date) VALUES (?, ?, ?, ?, ?, ?)',
-                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date))
-
-            Prev_ID = self.cursor.lastrowid
-
-            self.cursor.execute('INSERT INTO Ebooks (ID, editor, pages, synopsis) VALUES (?, ?, ?, ?)',
-                                (Prev_ID, document.editor, document.pages, document.synopsis))
-
-            self.cursor.execute('INSERT INTO Electronic_documents (ID, author, title, price, topic, language, pub_date, size, doi) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                (Prev_ID, document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi))
+            self.cursor.execute('INSERT INTO Ebooks (author, title, price, topic, language, pub_date, size, doi, editor, pages, synopsis) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi, document.editor, document.pages, document.synopsis))
             self.connection.commit()
 
             return "Document inserted successfully"
 
         elif isinstance(document, Magazine):
-
-            self.cursor.execute('INSERT INTO All_documents (author, title, price, topic, language, pub_date) VALUES (?, ?, ?, ?, ?, ?)',
-                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date))
-
-            Prev_ID = self.cursor.lastrowid
-
-            self.cursor.execute('INSERT INTO Magazines (ID, edition, pages) VALUES (?, ?, ?)',
-                                (Prev_ID, document.edition, document.pages))
-
-            self.cursor.execute('INSERT INTO Electronic_documents (ID, author, title, price, topic, language, pub_date, size, doi) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                (Prev_ID, document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi))
+            self.cursor.execute('INSERT INTO Magazines ( author, title, price, topic, language, pub_date, size, doi, edition, pages) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi, document.edition, document.pages))
             self.connection.commit()
 
             return "Document inserted successfully"
 
         elif isinstance(document, InvBook):
-
-            self.cursor.execute('INSERT INTO All_documents (author, title, price, topic, language, pub_date) VALUES (?, ?, ?, ?, ?, ?)',
-                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date))
-
-            Prev_ID = self.cursor.lastrowid
-
-            self.cursor.execute('INSERT INTO Investigation_books (ID, pages, abstract) VALUES (?, ?, ?)',
-                                (Prev_ID, document.pages, document.abstract))
-
-            self.cursor.execute('INSERT INTO Electronic_documents (ID, author, title, price, topic, language, pub_date, size, doi) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                (Prev_ID, document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi))
+            self.cursor.execute('INSERT INTO Investigation_books (author, title, price, topic, language, pub_date, size, doi, pages, abstract) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.size, document.doi, document.pages, document.abstract))
             self.connection.commit()
 
             return "Document inserted successfully"
 
         elif isinstance(document, Book):
-            self.cursor.execute('INSERT INTO All_documents (author, title, price, topic, language, pub_date) VALUES (?, ?, ?, ?, ?, ?)',
-                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date))
-
-            Prev_ID = self.cursor.lastrowid
-
-            self.cursor.execute('INSERT INTO Books (ID, editor, pages, synopsis, presentation) VALUES (?, ?, ?, ?, ?)',
-                                (Prev_ID, document.editor, document.pages, document.synopsis, document.presentation))
-
-            self.cursor.execute('INSERT INTO Physical_documents (ID,author, title, price, topic, language, pub_date, publisher) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                                (Prev_ID, document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.publisher))
+            self.cursor.execute('INSERT INTO Books (author, title, price, topic, language, pub_date, publisher, editor, pages, synopsis, presentation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                                (document.author, document.title, document.price, document.topic, document.language, document.pub_date, document.publisher, document.editor, document.pages, document.synopsis, document.presentation))
             self.connection.commit()
 
             return "Document inserted successfully"
 
-    def delete_document(self, ID_document: int):
+    def delete_document(self, ID_document: int, table_name: str):
 
-        self.cursor.execute(
-            'DELETE FROM Audiobooks WHERE ID = ?', (ID_document,))
-        self.cursor.execute('DELETE FROM Ebooks WHERE ID = ?', (ID_document,))
-        self.cursor.execute(
-            'DELETE FROM Investigation_books WHERE ID = ?', (ID_document,))
-        self.cursor.execute(
-            'DELETE FROM Magazines WHERE ID = ?', (ID_document,))
-        self.cursor.execute('DELETE FROM Books WHERE ID = ?', (ID_document,))
+        if table_name == "Audiobooks":
+            self.cursor.execute(
+                '''DELETE FROM Audiobooks WHERE ID = ?''', (ID_document,))
+            self.connection.commit()
+            return "Document deleted successfully"
 
-        self.cursor.execute(
-            'DELETE FROM Electronic_documents WHERE ID = ?', (ID_document,))
-        self.cursor.execute(
-            'DELETE FROM Physical_documents WHERE ID = ?', (ID_document,))
+        elif table_name == "Ebooks":
+            self.cursor.execute(
+                '''DELETE FROM Ebooks WHERE ID = ?''', (ID_document,))
+            self.connection.commit()
+            return "Document deleted successfully"
 
-        self.cursor.execute(
-            'DELETE FROM All_documents WHERE ID = ?', (ID_document,))
+        elif table_name == "Magazines":
+            self.cursor.execute(
+                '''DELETE FROM Magazines WHERE ID = ?''', (ID_document,))
+            self.connection.commit()
+            return "Document deleted successfully"
 
-        self.connection.commit()
-        return "Document deleted successfully"
+        elif table_name == "Investigation_books":
+            self.cursor.execute(
+                '''DELETE FROM Investigation_books WHERE ID = ?''', (ID_document,))
+            self.connection.commit()
+            return "Document deleted successfully"
+
+        elif table_name == "Books":
+            self.cursor.execute(
+                '''DELETE FROM Books WHERE ID = ?''', (ID_document,))
+            self.connection.commit()
+            return "Document deleted successfully"
+
+        else:
+            return "Document not found"
 
     def select_documents(self):
-        self.cursor.execute('''SELECT * FROM All_documents''')
+        self.cursor.execute(
+            '''SELECT * FROM Books, Audiobooks, Ebooks, Magazines, Investigation_books''')
+
         return self.cursor.fetchall()
 
 
