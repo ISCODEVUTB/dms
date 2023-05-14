ec3139d99d2e76807d584727aebee8207f71ed63
Adrian <avizcaino@utb.edu.co>
2023-05-14 13:13:20 -0500

    Change methods names

--- app.py
+++ app.py
@@ -37,16 +37,16 @@ async def root(table_name: str = ''):
 
 
 @app.post("/api/document/ElectronicDoc/Add/Book")
-async def add_Book(author: str = "author",
-                   title: str = "title", 
-                   price: float = 100.0, 
-                   topic: str = "topic", 
-                   language: str = "language", 
-                   pub_date: date = date.today(), 
-                   publisher: str = "publisher", 
-                   editor: str = "editor", 
-                   pages: int = 1, 
-                   synopsis: str = "synopsis", 
+async def add_book(author: str = "author",
+                   title: str = "title",
+                   price: float = 100.0,
+                   topic: str = "topic",
+                   language: str = "language",
+                   pub_date: date = date.today(),
+                   publisher: str = "publisher",
+                   editor: str = "editor",
+                   pages: int = 1,
+                   synopsis: str = "synopsis",
                    presentation: str = "presentation"):
     """
     Add a book to the database
@@ -67,15 +67,15 @@ async def add_Book(author: str = "author",
 
 
 @app.post("/api/document/ElectronicDoc/Add/Audiobook")
-async def add_Audiobook(author: str = "author",
-                        title: str = "title", 
-                        price: float = 100.0, 
-                        topic: str = "topic", 
-                        language: str = "language", 
-                        pub_date: date = date.today(), 
-                        size: float = 1, 
-                        doi: str = "doi", 
-                        duration: float = 1, 
+async def add_audiobook(author: str = "author",
+                        title: str = "title",
+                        price: float = 100.0,
+                        topic: str = "topic",
+                        language: str = "language",
+                        pub_date: date = date.today(),
+                        size: float = 1,
+                        doi: str = "doi",
+                        duration: float = 1,
                         synopsis: str = "synopsis"):
     """
     Add an audiobook to the database
@@ -95,16 +95,16 @@ async def add_Audiobook(author: str = "author",
 
 
 @app.post("/api/document/ElectronicDoc/Add/Ebook")
-async def add_Ebook(author: str = "author",
-                    title: str = "title", 
-                    price: float = 100.0, 
-                    topic: str = "topic", 
-                    language: str = "language", 
-                    pub_date: date = date.today(), 
-                    size: float = 1, 
-                    doi: str = "doi", 
-                    editor: str = "editor", 
-                    pages: int = 1, 
+async def add_ebook(author: str = "author",
+                    title: str = "title",
+                    price: float = 100.0,
+                    topic: str = "topic",
+                    language: str = "language",
+                    pub_date: date = date.today(),
+                    size: float = 1,
+                    doi: str = "doi",
+                    editor: str = "editor",
+                    pages: int = 1,
                     synopsis: str = "synopsis"):
     """
     Add an ebook to the database
@@ -125,15 +125,15 @@ async def add_Ebook(author: str = "author",
 
 
 @app.post("/api/document/ElectronicDoc/Add/Invbook")
-async def add_Invbook(author: str = "author",
-                      title: str = "title", 
-                      price: float = 100.0, 
-                      topic: str = "topic", 
-                      language: str = "language", 
-                      pub_date: date = date.today(), 
-                      size: float = 1, 
-                      doi: str = "doi", 
-                      pages: int = 1, 
+async def add_invbook(author: str = "author",
+                      title: str = "title",
+                      price: float = 100.0,
+                      topic: str = "topic",
+                      language: str = "language",
+                      pub_date: date = date.today(),
+                      size: float = 1,
+                      doi: str = "doi",
+                      pages: int = 1,
                       abstract: str = "abstract"):
     """
     Add an investigation book to the database
@@ -153,15 +153,15 @@ async def add_Invbook(author: str = "author",
 
 
 @app.post("/api/document/ElectronicDoc/Add/Magazine")
-async def add_Magazine(author: str = "author",
-                       title: str = "title", 
-                       price: float = 100.0, 
-                       topic: str = "topic", 
-                       language: str = "language", 
-                       pub_date: date = date.today(), 
-                       size: float = 1, 
-                       doi: str = "doi", 
-                       edition: int = 1, 
+async def add_magazine(author: str = "author",
+                       title: str = "title",
+                       price: float = 100.0,
+                       topic: str = "topic",
+                       language: str = "language",
+                       pub_date: date = date.today(),
+                       size: float = 1,
+                       doi: str = "doi",
+                       edition: int = 1,
                        pages: int = 1):
     """
     Add a magazine to the database
